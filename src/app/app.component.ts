7b91fe33d08c5850e91ad9e4cd3446320556ca87
Administrador <hitalo.emanoel@gmail.com>
2019-01-15 17:04:22 -0300

    sidebar collapse collapse with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutService } from './services/layout-service/layout.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'webview';
+
+  active: boolean;
+  $active: Observable<boolean>;
+
+  constructor(private layoutService: LayoutService) {}
+
+  ngOnInit() {
+    this.$active = this.layoutService.$active;
+
+    this.$active.subscribe(active => {
+      this.active = active;
+    });
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from '../services/layout-service/layout.service';
 
 @Component({
   selector: 'navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private layoutService: LayoutService) { }
 
   ngOnInit() {
   }
 
+
+  toogleSideBar() {
+    this.layoutService.toogleSideBar();
+  }
 }
