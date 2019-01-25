3a2eb77c8d7fc36a3f0f7a659de283e1e184807c
Administrador <hitalo.emanoel@gmail.com>
2019-01-25 11:59:19 -0300

    a few comments in carousel added

--- src/app/corousel/corousel.component.ts
+++ src/app/corousel/corousel.component.ts
@@ -1,19 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+interface carousel {
+  img: string;
+  title: string;
+  subtitle: string;
+  active?: boolean;
+}
 
 @Component({
   selector: 'app-corousel',
   templateUrl: './corousel.component.html',
   styleUrls: ['./corousel.component.scss']
 })
-export class CorouselComponent implements OnInit {
+export class CorouselComponent implements AfterViewInit {
 
   slide1: string = "assets/first.png";
   slide2: string = "assets/second.png";
   slide3: string = "assets/third.png";
 
+  // placeholders: carousel[] = [{
+  //   img: "assets/first.png",
+  //   title: "First Slide",
+  //   subtitle: "Such a boring slide!!!",
+  //   active: true
+  // }, {
+  //   img: "assets/second.png",
+  //   title: "Second Slide",
+  //   subtitle: "... at least this one is in another color!!!"
+  // }, {
+  //   img: "assets/third.png",
+  //   title: "Third Slide",
+  //   subtitle: "... ok I am done!!!"
+  // }];
+
   constructor() { }
 
-  ngOnInit() {
+
+  ngAfterViewInit() {
+    // let car0 = document.getElementById("car0");
+    // car0.classList.add("active");
   }
 
 }
