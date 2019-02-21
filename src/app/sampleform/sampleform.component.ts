ace0c4c80c0a556a2f5b629aae14e9ebf5e1f2f4
Administrador <hitalo.emanoel@gmail.com>
2019-02-21 20:14:02 -0300

    fixed check validator on sampleform

--- src/app/sampleform/sampleform.component.ts
+++ src/app/sampleform/sampleform.component.ts
@@ -10,6 +10,7 @@ export class SampleformComponent implements OnInit {
 
   formData: FormGroup;
   private minPassLength: number = 6;
+  private checkError: boolean;
 
   constructor() {}
 
@@ -18,16 +19,19 @@ export class SampleformComponent implements OnInit {
     this.formData = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       pass: new FormControl('', [Validators.required, Validators.minLength(this.minPassLength),
-        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.$@$!%*?&])[A-Za-z\d$@$!%*?&].{'+ this.minPassLength +',}')])
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.$@$!%*?&])[A-Za-z\d$@$!%*?&].{'+ this.minPassLength +',}')]),
+      check: new FormControl('', Validators.requiredTrue)
    }); 
 
   }
 
   submitForm() {
     console.log(this.formData.value);
+    console.log(this.check.errors);
   }
 
   get email() { return this.formData.get('email'); }
   get pass() { return this.formData.get('pass'); }
+  get check() { return this.formData.get('check'); }
 
 }
