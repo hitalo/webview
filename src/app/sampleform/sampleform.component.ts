0ff5d0e5f60ad3fc9a6dfea96d8e7045891f485b
Administrador <hitalo.emanoel@gmail.com>
2019-02-22 19:57:34 -0300

    states added in sampleform

--- src/app/sampleform/sampleform.component.ts
+++ src/app/sampleform/sampleform.component.ts
@@ -10,7 +10,7 @@ export class SampleformComponent implements OnInit {
 
   formData: FormGroup;
   private minPassLength: number = 6;
-  private checkError: boolean;
+  private states: string[] = ['RN', 'CE', 'PB', 'PE'];
 
   constructor() {}
 
@@ -19,8 +19,9 @@ export class SampleformComponent implements OnInit {
     this.formData = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       pass: new FormControl('', [Validators.required, Validators.minLength(this.minPassLength),
-        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.$@$!%*?&])[A-Za-z\d$@$!%*?&].{'+ this.minPassLength +',}')]),
-      check: new FormControl('', Validators.requiredTrue)
+        Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[.$@$!%*?&])[A-Za-z\d$@$!%*?&].{'+ (this.minPassLength - 1) +',}')]),
+      check: new FormControl('', Validators.requiredTrue),
+      state: new FormControl(''),
    }); 
 
   }
@@ -33,5 +34,6 @@ export class SampleformComponent implements OnInit {
   get email() { return this.formData.get('email'); }
   get pass() { return this.formData.get('pass'); }
   get check() { return this.formData.get('check'); }
+  get state() { return this.formData.get('state'); }
 
 }
